bbea63fe4ad51611f5cfaf91dc53561277a04cc5
My Le <mimile@Mys-MacBook-Pro.local>
2022-09-14 16:02:48 -0600

    oauth

--- server/server.ts
+++ server/server.ts
@@ -7,6 +7,8 @@ const interviewRouter = require('./routes/interviewRouter');
 const researchRouter = require('./routes/researchRouter');
 const userRouter = require('./routes/userRouter');
 
+const axios = require('axios');
+
 const PORT = 3000;
 
 const app = express();
@@ -24,11 +26,47 @@ app.use('/interview', interviewRouter);
 app.use('/research', researchRouter);
 app.use('/user', userRouter);
 
+let access_token = "";
+
+const clientID = '03816ddd24e96db22b7c';
+
+const clientSecret = '5249bed44c6a51eabd3dd0ae182fa03650f132c8';
+
+app.set('view engine', 'ejs');
+
 /**
  * root
  */
 app.get('/', (req: any, res: any) => {
-  res.sendFile(path.resolve(__dirname, '../src/index.html'))
+  res.render(path.resolve(__dirname, '../src/index.html'), {client_id: clientID});
+});
+
+app.get('/login/oauth', (req: any, res: any) => {
+  const requestToken = req.query.code;
+  axios({
+    method: 'post',
+    url: `https://github.com/login/oauth/access_token?client_id=${clientID}&client_secret
+    =${requestToken}`,
+    headers: {
+      accept: 'application/json'
+    }
+  }).then((res: any) => {
+    access_token = res.data.access_token
+    res.redirect('/success');
+  })
+
+})
+
+app.get('/success', (req: any, res: any) => {
+  axios({
+    method: 'get',
+    url: `https://api.github/com/user`,
+    headers: {
+      Authorization: 'token ' + access_token
+    }
+  }).then((response: any) => {
+    res.render('pages/sucess', { userData: response.data })
+  })
 });
 
 /**
